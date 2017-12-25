000bdca8b9168ab11e67fbceab933093c158b2ff
Khinenw <deu07115@gmail.com>
2017-12-25 14:54:59 +0900

    Updated for electron

---
+++ index.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow, protocol} = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+	protocol.registerFileProtocol('kakinpos', (req, cb) => {
+		const requestPath = req.url
+			.replace(/^kakinpos:\/\/kakinpos\//, '')
+			.replace(/\?.*/, '').replace(/\#.*/, '');
+
+		if(requestPath === '') {
+			cb(path.resolve(__dirname, 'dist', 'index.html'));
+			return;
+		}
+
+		const fullUrl = path.resolve(__dirname, requestPath);
+		cb(fullUrl);
+	});
+
+	mainWindow = new BrowserWindow({
+		width: 1280,
+		height: 900
+	});
+	mainWindow.setMenu(null);
+	mainWindow.loadURL(`kakinpos://kakinpos/`);
+	mainWindow.toggleDevTools();
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+});
+
+app.on('window-all-closed', () => {
+	app.quit()
+});
